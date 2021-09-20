1e84432c42e09fff003632e15c3a8b17011b61a7
Joe Richardson <joeyrichardson96@gmail.com>
2021-09-21 00:37:17 +0100

    Express JS Backend commited

---
+++ ProjectPowerApi/BLL/UserLogin/loginHandler.js
@@ -0,0 +1,70 @@
+var db = require('../../DAL/database')
+var A2sFl = require('../FormulaLibrary/Average 2 Savage/A2SFormulaLibrary')
+const bcrypt = require('bcrypt')
+const e = require('express');
+const { json } = require('express');
+const saltRounds = 10
+global.activeAccount = "";
+global.currentWeek = 1;
+global.currentDay = 1;
+
+ function createPassword()
+ {
+     bcrypt.genSalt(saltRounds, function (err, salt) {
+         bcrypt.hash(password, salt, function (err, hash) {
+             db.saveUsernameAndPassword(username, hash);
+         });
+     });
+ }
+
+ async function login(loginInfo)
+ {
+     if(authenticatePassword(loginInfo.username,loginInfo.password)){
+         setActiveAccount(loginInfo.username)
+        return true;
+    }
+    else{
+         console.log("Login failed");
+        return false
+    }
+ }
+
+async function authenticatePassword(username, password)
+{
+    var hashedPw = await db.getPassword(username)
+    bcrypt.compare(hashedPw, password, function (err, result) {
+        try {
+            if(result){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        catch (err) {
+            throw (err);
+        }
+    })
+}
+
+async function setActiveAccount(username) {
+    var acc = await db.getUserId(username);
+    var weekAndDay = await db.getCurrentUserWeekAndDay(activeAccount)
+    
+    activeAccount = acc[0][0].UserId;
+    currentWeek = weekAndDay.recordset[0].CurrentWeek;
+    currentDay = weekAndDay.recordset[0].CurrentDay;
+
+    var x = await db.returnWeeklyWorkout(activeAccount,currentWeek)
+    console.log(currentWeek);
+}
+
+
+module.exports = {
+    activeAccount: this.activeAccount,
+    currentWeek: this.currentWeek,
+    currentDay: this.currentDay,
+    login:login
+}
+
+
---
+++ ProjectPowerApi/DAL/database.js
@@ -0,0 +1,165 @@
+var sql = require('mssql');
+var EXE = require('../API/exercise')
+
+config = {
+    server: "DESKTOP-72FH6UA",
+    user: "test",
+    password: "test",
+    database: "ProjectPower",
+    trustServerCertificate: true
+}
+
+async function updatePassword(username,newPassword){
+    let pool = await sql.connect(config);
+    let updatedPassword = await pool.request()
+        .input('UserName', sql.NVarChar, username)
+        .input('Password', sql.NVarChar, newPassword)
+        .execute('UpdatePassword');
+    return updatedPassword;
+}
+
+async function saveUsernameAndPassword(username,password) {
+    try {
+        let pool = await sql.connect(config);
+        let exercise = await pool.request()
+            .input('UserName', sql.NVarChar, username)
+            .input('Password', sql.NVarChar, password)
+            .execute('AddUserAndPasswordToDb');
+        return exercise.recordsets;
+    }
+    catch (err) {
+        console.log(err);
+    }
+}
+async function getPassword(username){
+    
+ let pool = await sql.connect(config);
+ let password = await pool.request()
+     .input('inputField1', sql.NVarChar, username)
+     .query("SELECT Password from UserAccounts WHERE UserName = @inputField1 ")
+ return password;
+ 
+}
+
+async function getExercises() {
+    try {
+        let pool = await sql.connect(config);
+        let exercises = await pool.request()
+        .query("SELECT * from ExerciseBase")
+
+        return exercises.recordsets
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+async function getExercise(name) {
+    try {
+        let pool = await sql.connect(config);
+        let exercise = await pool.request()
+        .input('inputField1', sql.NVarChar, name)
+        .query("SELECT * from ExerciseBase Where Name = @inputField1 ");
+ 
+        return exercise.recordsets;
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+async function postExercise(exe) {
+    try {
+        let pool = await sql.connect(config);
+        let exercise = await pool.request()
+            .input('Name', sql.NVarChar, exe.name)
+            .input('Template', sql.NVarChar, exe.template)
+            .input('TrainingMax', sql.Decimal, exe.trainingMax)
+            .execute('AddExerciseToDB');
+
+        return exercise.recordsets;
+    }
+    catch (err) {
+        console.log(err);
+    }
+}
+
+async function returnWeekIntensity(exerciseName, currentWeek){
+    try{
+        let pool = await sql.connect(config);
+        let weekIntensityTm = await pool.request()
+            .input('Week', sql.Int, currentWeek)
+            .input('ExerciseName', sql.NVarChar, exerciseName)
+            .execute('ReturnWeekIntensity');
+            return weekIntensityTm;
+    }
+    catch(err){
+        console.log(err);
+    }
+}
+
+async function returnWeeklyWorkout(userid, currentWeek) {
+    try {
+        let pool = await sql.connect(config);
+        let weeklyWorkout = await pool.request()
+            .input('CurrentWeek', sql.Int, currentWeek)
+            .input('UserId', sql.NVarChar, userid)
+            .execute('GetWorkoutExercises');
+
+        
+            var transformedList = await EXE.returnWeight(weeklyWorkout);
+        
+        return transformedList;
+    }
+    catch (err) {
+        console.log(err);
+    }
+}
+
+async function getExerciseTemplate(name) {
+    try {
+        let pool = await sql.connect(config);
+        let exercise = await pool.request().input('inputField1', sql.NVarChar, name)
+        .query("SELECT * FROM A2SHyperTrophyTemplate WHERE Week = 1");
+
+        return exercise.recordsets;
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+async function getCurrentUserWeekAndDay(userId){
+    try{
+        let pool = await sql.connect(config);
+        let weekAndDay = await pool.request()
+        .input('inputField1', sql.Int, userId)
+        .query('SELECT CurrentWeek,CurrentDay FROM UserGlobalInfo WHERE UserId = @inputField1');
+        return weekAndDay;
+    }
+    catch(err){
+        throw(err);
+    }
+}
+
+async function getUserId(username){
+    let pool = await sql.connect(config);
+    let userId = await pool.request()
+    .input('inputField1', sql.NVarChar, username)
+    .query('SELECT UserId FROM UserAccounts WHERE UserName = @inputField1');
+    return userId.recordsets;
+}
+
+module.exports = {
+    getCurrentUserWeekAndDay: getCurrentUserWeekAndDay,
+    getExercises: getExercises,
+    getExercise: getExercise,
+    postExercise: postExercise,
+    getExerciseTemplate: getExerciseTemplate,
+    updatePassword: updatePassword,
+    saveUsernameAndPassword: saveUsernameAndPassword,
+    getPassword: getPassword,
+    returnWeekIntensity: returnWeekIntensity,
+    getUserId: getUserId,
+    returnWeeklyWorkout: returnWeeklyWorkout
+}
