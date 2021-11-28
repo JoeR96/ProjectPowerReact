8e13a04ee31cb68f51a25babb2af099b9830b75a
FIRST_NAME LAST_NAMEgit config --global user.name FIRST_NAME <joeyrichardson96@gmail.com>
2021-11-28 20:45:47 +0000

    commit

--- ProjectPower/src/components/WorkoutCreation/WorkoutCreation.js
+++ ProjectPower/src/components/WorkoutCreation/WorkoutCreation.js
@@ -1,7 +1,9 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useContext } from "react";
 import { Grid, Box } from "@material-ui/core";
 import Reactdnd1 from "../Cards/Reactdnd1";
 import ExerciseCreation from "../ScaffoldA2SExerciseForm/ExerciseCreation";
+import { createContext } from 'react';
+
 function DragList() {
   function UpdateState(value) {
     console.log(value);
@@ -12,25 +14,31 @@ function DragList() {
   function updateName(name) {
     console.log(name);
   }
+  const Context = createContext({});
   const cref = useRef();
   const [exercises, setExercises] = useState([]);
+  const [newExercise, setNewExercise] = useState({});
 
   return (
-    <div>
-      <Grid container>
-        <Grid item md={2}>
-          <Box pt={6.25} >
-            <ExerciseCreation
-              sendName={updateName}
-              submitExercise={UpdateState}
-            ></ExerciseCreation>
-          </Box>
+    <Context.Provider value={exercises}>
+
+      <div>
+        <Grid container>
+          <Grid item md={2}>
+            <Box pt={6.25} >
+              <ExerciseCreation
+                sendName={updateName}
+                submitExercise={UpdateState}
+              ></ExerciseCreation>
+            </Box>
+          </Grid>
+          <Grid item md={10}>
+            <Reactdnd1 ref={cref}></Reactdnd1>
+          </Grid>
         </Grid>
-        <Grid item md={10}>
-          <Reactdnd1 ref={cref}></Reactdnd1>
-        </Grid>
-      </Grid>
-    </div>
+      </div>
+    </Context.Provider >
+
   );
 }
 export default DragList;
